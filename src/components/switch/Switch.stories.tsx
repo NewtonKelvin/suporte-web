e6142b8a067a48c977d88591f42b6a6be6acd1d0
Kelvin Newton <kelvinnewtonsm@gmail.com>
2023-06-24 15:42:55 -0300

    remove unecessary components

--- src/components/switch/Switch.stories.tsx
+++ src/components/switch/Switch.stories.tsx
@@ -1,14 +1,14 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import CustomSwitch from "./Switch";
+import { default as Switch } from "./Switch";
 
-const meta: Meta<typeof CustomSwitch> = {
-  component: CustomSwitch,
+const meta: Meta<typeof Switch> = {
+  component: Switch,
   title: "Components/Switch"
 };
 
 export default meta;
-type Story = StoryObj<typeof CustomSwitch>;
+type Story = StoryObj<typeof Switch>;
 
 export const Default: Story = {
   args: {
